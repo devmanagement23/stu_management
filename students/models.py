fb200cf5c347910b170d61d8392fb68c4572b9c2
devmanagement23 <devmanagement23@gmail.com>
2023-08-31 08:46:58 +0530

    4th commit - Add and View done

--- students/models.py
+++ students/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):  
+    roll=models.CharField(max_length=100)
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=150)
--- students/views.py
+++ students/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Student
 
 # Create your views here.
+
+
+# def home (request):
+#     return render(request,"students/home.html",)
+
 def home (request):
-    return render(request,"students/home.html",)
+
+    totalstudent = Student.objects.all()
+
+    return render(request,"students/home.html",{'studentall':totalstudent})
+
 
 def add_student(request):
+    if request.method=='POST':
+        print("Added")
+        #retreive the user inputs
+        student_roll = request.POST.get("std_roll")
+        student_name = request.POST.get("std_name")
+        student_email = request.POST.get("std_email")
+
+        #create an object for models
+        s=Student()
+        s.roll = student_roll
+        s.name = student_name
+        s.email = student_email
+
+        s.save()
+
+        return redirect("/students/home")
+
+    # this bleow return line is like  ELSE part.
     return render(request,"students/add_student.html")
